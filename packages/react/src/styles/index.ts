69b53aa8d475f355e1c57947ced3311059cb8faa
Paulo Cezar Motta <paulocezarmotta@gmail.com>
2023-07-08 19:03:25 -0300

    Commit inicial

---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,27 @@
+import {createStitches, defaultThemeMap} from "@stitches/react";
+import {colors, fonts, fontSizes, fontWeights, lineHeights, radii, space} from "@ignite-ui/tokens";
+
+export const {
+    styled,
+    css,
+    globalCss,
+    keyframes,
+    getCssText,
+    createTheme,
+    config
+} = createStitches({
+    themeMap: {
+        ...defaultThemeMap,
+        height: 'space',
+        width: 'space',
+    },
+    theme: {
+        colors,
+        fontSizes,
+        fontWeights,
+        fonts,
+        lineHeights,
+        radii,
+        space,
+    }
+})
